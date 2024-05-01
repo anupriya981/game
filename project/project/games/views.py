f1efb0325ce874ea74f059dbc7894f0ad941c8c1
anupriya <anupriyaa981@gmail.com>
2024-05-01 10:58:16 +0530

    updates

--- project/project/games/views.py
+++ project/project/games/views.py
@@ -6,7 +6,7 @@ def index(request):
     # feature_product=games.objects.order_by('priority') 
     return render(request, 'index.html')
 
-def list_games(request):
+def list_games(request,p):
     """_summary_
     returns games list page
     args:
@@ -14,14 +14,12 @@ def list_games(request):
     Returns:
         _type_: _description_
     """
-    # games_list=games.objects.all()
-    # context={"games": games_list}
-    return render(request, 'games.html')
-
-def detail_games(request,pk):
-    game=games.objects.get(pk=pk)
-    context={' game':game}
+    games_list=games.objects.get(pk=p)
+    context={"games": games_list}
     return render(request, 'games.html',context)
+
+def detail_games(request): 
+    return render(request, 'games.html')
 
 def description(request):
     return render(request, 'games_description_car.html')
@@ -48,7 +46,9 @@ def description_gtav(request):
     return render(request, 'game_des_gta.html')
 
 def game_list(request):
-    return render(request, 'games_list_page.html')
+    g=games.objects.all()
+    context={'games':g}
+    return render(request, 'games_list_page.html',context)
 
 
 
